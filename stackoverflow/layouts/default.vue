7a6fe87cbbfe5be0f6b605071692c3def8645db8
SanderVHGD <sander.vhgd@gmail.com>
2021-10-02 22:34:47 +0200

    Frontend setup

---
+++ stackoverflow/layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="container">
+        <div class="navbar-brand">
+          <a
+            class="navbar-item"
+            href="/"
+          >
+            Stackoverflow
+          </a>
+
+          <div class="navbar-burger">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <div class="container">
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ stackoverflow/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div class="column">
+        <button>Ask a question</button>
+      </div>
+      <div class="column card">
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">Save</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+
+
+}
+</script>
